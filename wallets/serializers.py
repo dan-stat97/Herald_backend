75af414a1ef0eec45bdce66c7fa23bb57dd8f04b
Joel <somtochukwujoel403@@gmail.com>
2026-03-03 22:10:01 +0100

    Fix WalletSerializer: explicitly handle user_id ForeignKey serialization

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Wallet
 
 class WalletSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    
     class Meta:
         model = Wallet
         fields = [
