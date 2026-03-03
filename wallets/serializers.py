eee0aae50c56a6e02cb53a7bc2df3e40e622f1b2
Joel <somtochukwujoel403@@gmail.com>
2026-03-03 22:16:40 +0100

    Fix WalletSerializer: use SerializerMethodField for user_id to properly serialize UUID

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -2,11 +2,18 @@ from rest_framework import serializers
 from .models import Wallet
 
 class WalletSerializer(serializers.ModelSerializer):
-    user_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    user_id = serializers.SerializerMethodField()
     
     class Meta:
         model = Wallet
         fields = [
             'id', 'user_id', 'httn_points', 'httn_tokens', 'espees', 'pending_rewards', 'created_at', 'updated_at'
         ]
-        read_only_fields = fields
+        read_only_fields = ['id', 'created_at', 'updated_at']
+    
+    def get_user_id(self, obj):
+        """Return the UUID of the user profile"""
+        try:
+            return str(obj.user_id.id) if obj.user_id else None
+        except:
+            return None
